c0c2162636af79832eac58047dcfbd42cd6db1a3
Matheus <matheus_romeike@sisand.com.br>
2024-03-08 22:01:12 -0300

    add ngbootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,12 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HttpInterceptorService } from './shared/interceptors/http.interceptor.service';
 import { SharedModule } from './shared/shared.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 registerLocaleData(localePt);
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, CoreModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, CoreModule, NgbModule],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
